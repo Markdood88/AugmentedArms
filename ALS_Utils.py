0bbe2c60be9cc28e050de1208c085c6f630b8c4d
Mark Armstrong <dudelookslikealady123@gmail.com>
2025-06-10 20:06:41 +0900

    Speaker Class Implementation

--- ALS_Utils.py
+++ ALS_Utils.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as GPIO
 import pygame
+import threading
 
 # Setup GPIO only once
 GPIO.setmode(GPIO.BCM)
@@ -33,17 +34,63 @@ class PiezoSensor:
 		return pressed
 
 class Speaker:
-	def __init__(self):
-		pygame.mixer.init()
-		self.channel = None
+	def __init__(self, volume=1.0):
+		self.channel = pygame.mixer.Channel(0)
+		self.queue = []
+		self.playing = False
+		self._lock = threading.Lock()
+		self._running = True
+		self.volume = volume  # volume float 0.0 to 1.0
+		self._thread = threading.Thread(target=self._update_loop, daemon=True)
+		self._thread.start()
 
-	def play(self, filename):
-		sound = pygame.mixer.Sound(filename)
-		self.channel = sound.play()
+	def play_single(self, filename, volume=None):
+		with self._lock:
+			sound = pygame.mixer.Sound(filename)
+			sound.set_volume(volume if volume is not None else self.volume)
+			self.channel.play(sound)
+			self.playing = True
 
-	def is_playing(self):
-		return self.channel is not None and self.channel.get_busy()
+	def play_overlap(self, filename, volume=None):
+		with self._lock:
+			sound = pygame.mixer.Sound(filename)
+			sound.set_volume(volume if volume is not None else self.volume)
+			channel = pygame.mixer.find_channel()
+			if channel is not None:
+				channel.play(sound)
+			else:
+				print("No free channel to play sound!")
+
+	def play_sequence(self, filenames, volume=None):
+		with self._lock:
+			if not self.playing:
+				self.queue = list(filenames)
+				self._play_next(volume)
+
+	def _play_next(self, volume=None):
+		if self.queue:
+			filename = self.queue.pop(0)
+			sound = pygame.mixer.Sound(filename)
+			sound.set_volume(volume if volume is not None else self.volume)
+			self.channel.play(sound)
+			self.playing = True
+		else:
+			self.playing = False
+
+	def _update_loop(self):
+		while self._running:
+			with self._lock:
+				if self.playing and not self.channel.get_busy():
+					self._play_next()
+			time.sleep(0.05)
 
 	def stop(self):
-		if self.channel:
-			self.channel.stop()
+		with self._lock:
+			if self.channel:
+				self.channel.stop()
+			self.queue.clear()
+			self.playing = False
+
+	def shutdown(self):
+		self._running = False
+		self._thread.join()
